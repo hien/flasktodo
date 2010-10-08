bb09e026533b5b1513a480ad8a2273700d6ece7b
Francisco Souza <francisco@franciscosouza.net>
2010-10-08 09:12:30 -0300

    Changed template for do ajax requests when adding new task

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,5 +1,16 @@
+function generate_li_for_task(task) {
+    var li = '<li id="'+ task.id + '"><a href="/done/' + task.id + '">X</a> ' + task.name;
+    li += ' <a href="/delete/' + task.id + '" onclick="delete_task(' + task.id + '); return false;">delete</a>';
+    return li;
+}
+
+function ajax_load(selector) {
+    jQuery(selector).html('<img src="/static/images/loader.gif" />');
+
+}
+
 function delete_task(id) {
-    jQuery('#loading-' + id).html('<img src="/static/images/loader.gif" />');
+    ajax_load("#loading-" + id);
     jQuery.get('/delete/' + id + '.json',
         {},
         function (data) {
@@ -7,4 +18,22 @@ function delete_task(id) {
             jQuery('#message-panel').html(data.message);
         },
     'json');
+}
+
+function add_task() {
+    var name = jQuery("#task-name").val();
+    jQuery("#button-add").hide();
+    ajax_load("#loading-add");
+    jQuery.post('/',
+        {
+            'format' : 'json',
+            'name' : name
+        },
+        function (data) {
+            jQuery("#button-add").show();
+            jQuery("#loading-add").html('');
+            jQuery("#message-panel").html(data.message);
+            jQuery("#task-list").append(generate_li_for_task(data.task));
+        },
+    'json')
 }
